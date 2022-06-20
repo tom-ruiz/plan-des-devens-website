412eb869e345af968fb0c56378785916276d4939
Tom <tomruiz2611@gmail.com>
2022-06-20 02:03:47 +0200

    fix ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import "../style/navbar.scss";
 import { AiOutlineMenu } from "react-icons/ai";
