4b87943ce4ff8dac284e28cf1e19835da5bacba9
tom-ruiz <tomruiz2611@gmail.com>
2022-10-29 17:20:56 +0200

    redirection home

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <NavbarComponent />
       <div className="container">
         <Routes>
+          <Route path ="/" element={<Home /> }/>
           <Route path ="/accueil" element={<Home /> }/>
           <Route path ="/plan-des-devens-website" element={<Home /> }/>
           <Route path ="/partners" element={<Partners /> }/>
